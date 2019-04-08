e10fac84e96abcfca831814c5b9b2a6f6dc99b75
Dávid Sörös <soros.david.009@gmail.com>
2019-04-08 06:46:43 -0700

    Sensor unit tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -6,14 +6,50 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
-
+    
+    TrainController controller;
+    TrainUser user;
+    TrainSensor sensor;
+    
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensor(controller, user);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void WithinMargins() {
+        when(controller.getReferencespeed()).thenReturn(100);
+        when(user.getAlarmState()).thenReturn(false);
+        sensor.overrideSpeedLimit(100);
+        verify(user,times(0)).setAlarmState(true);
+    }
+    
+    
+    @Test
+    public void OutOfAbsoluteMargins() {
+        when(controller.getReferencespeed()).thenReturn(400);
+        when(user.getAlarmState()).thenReturn(true);
+        sensor.overrideSpeedLimit(400);
+        verify(user,times(1)).setAlarmState(true);
+    }
+    
+    
+    @Test
+    public void OutOfRelativeMargins() {
+        when(controller.getReferencespeed()).thenReturn(100);
+        when(user.getAlarmState()).thenReturn(true);
+        sensor.overrideSpeedLimit(50);
+        verify(user,times(1)).setAlarmState(true);
+    }
+    
+    
+    @Test
+    public void OutOfBothMargins() {
+        when(controller.getReferencespeed()).thenReturn(100);
+        when(user.getAlarmState()).thenReturn(true);
+        sensor.overrideSpeedLimit(-500)
+        verify(user,times(1)).setAlarmState(true);
     }
 }
